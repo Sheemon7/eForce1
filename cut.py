057fd170c01444043af9dee181040e7768f4b17a
Simon Mandlik <simon.mandlik@gmail.com>
2019-11-21 17:28:25 +0100

    ...

--- cut.py
+++ cut.py
@@ -4,14 +4,14 @@ import cv2
 import time
 
 def find_lines_and_center(img, ind_1=130, ind_2=190):
-	img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-	img = img[ind_1:ind_2, :]
-	suma = np.sum(img, axis=0)
-	left_line = max(0, np.argmin(suma[:120]))
-	right_line = 120+ min(120,np.argmin(suma[120:]))
-	stred = (right_line+left_line)/2
-	odchylka = stred-120
-	return odchylka, stred, left_line, right_line, img
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = img[ind_1:ind_2, :]
+    suma = np.sum(img, axis=0)
+    left_line = max(0, np.argmin(suma[:120]))
+    right_line = 120+ min(120,np.argmin(suma[120:]))
+    stred = (right_line+left_line)/2
+    odchylka = stred-120
+    return odchylka, stred, left_line, right_line, img
 
 """
 img = np.load("test_image.npy")
