ca6ec4ff0b7e6c1d9763bea2c73b50fb6686623e
Simon Mandlik <simon.mandlik@gmail.com>
2019-11-21 12:05:23 +0000

    up

--- camera_example.py
+++ camera_example.py
@@ -15,9 +15,11 @@ rawCapture = PiRGBArray(camera, size=(320, 240))
 
 #Let camera warm up
 time.sleep(0.2)
-
+tmp = 0
+#start = time.time()
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
 	img = frame.array
+	#end = time.time()
 	cv2.imshow("Preview", img)
 	
 	rawCapture.truncate(0)
@@ -26,7 +28,10 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
 	if key == ord("q"):
 		print("Quitting")
 		break
-	
+	if tmp==0:
+		np.save("test_image.npy",img)
+	#	print(end-start)
+		tmp+=1
 	
 cv2.destroyAllWindows()
 camera.close()
