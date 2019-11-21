194bb3cdb1e383cd9a148843f71a409ab8a508d5
Simon Mandlik <simon.mandlik@gmail.com>
2019-11-21 16:05:29 +0000

    uppp

--- PEC_PWM.py
+++ PEC_PWM.py
@@ -43,30 +43,30 @@ SW_GPIO = 26
 
 
 def setup_gpios():
-    # Setup GPIOs
-    wiringpi.wiringPiSetupGpio()
-    
-    wiringpi.pinMode(LED_GPIO, wiringpi.GPIO.OUTPUT)
-    wiringpi.digitalWrite(LED_GPIO, wiringpi.GPIO.OUTPUT)
-    
-    wiringpi.pinMode(MOTOR_SPL_EN_GPIO, wiringpi.GPIO.OUTPUT)
-    
-    wiringpi.pinMode(MOTOR_DIR_GPIO, wiringpi.GPIO.OUTPUT)
-    wiringpi.pinMode(MOTOR_DISABLE_GPIO, wiringpi.GPIO.OUTPUT)
-    
-    wiringpi.pinMode(MOTOR_PWM_GPIO, wiringpi.GPIO.PWM_OUTPUT)
-    wiringpi.pinMode(SERVO_PWM_GPIO, wiringpi.GPIO.PWM_OUTPUT)
-    
-    wiringpi.pwmSetMode(wiringpi.GPIO.PWM_MODE_MS)
-    wiringpi.pwmSetClock(192)
-    wiringpi.pwmSetRange(2000)
-    
-    wiringpi.pinMode(SONIC_ECHO_GPIO, wiringpi.GPIO.INPUT)
-    wiringpi.pinMode(SONIC_TRIG_GPIO, wiringpi.GPIO.OUTPUT)
-    
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(SW_GPIO, GPIO.IN)
-    GPIO.add_event_detect(SW_GPIO, GPIO.FALLING, button_pressed, 200)
+	# Setup GPIOs
+	wiringpi.wiringPiSetupGpio()
+
+	wiringpi.pinMode(LED_GPIO, wiringpi.GPIO.OUTPUT)
+	wiringpi.digitalWrite(LED_GPIO, wiringpi.GPIO.OUTPUT)
+
+	wiringpi.pinMode(MOTOR_SPL_EN_GPIO, wiringpi.GPIO.OUTPUT)
+
+	wiringpi.pinMode(MOTOR_DIR_GPIO, wiringpi.GPIO.OUTPUT)
+	wiringpi.pinMode(MOTOR_DISABLE_GPIO, wiringpi.GPIO.OUTPUT)
+
+	wiringpi.pinMode(MOTOR_PWM_GPIO, wiringpi.GPIO.PWM_OUTPUT)
+	wiringpi.pinMode(SERVO_PWM_GPIO, wiringpi.GPIO.PWM_OUTPUT)
+
+	wiringpi.pwmSetMode(wiringpi.GPIO.PWM_MODE_MS)
+	wiringpi.pwmSetClock(192)
+	wiringpi.pwmSetRange(2000)
+
+	wiringpi.pinMode(SONIC_ECHO_GPIO, wiringpi.GPIO.INPUT)
+	wiringpi.pinMode(SONIC_TRIG_GPIO, wiringpi.GPIO.OUTPUT)
+
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setup(SW_GPIO, GPIO.IN)
+	GPIO.add_event_detect(SW_GPIO, GPIO.FALLING, button_pressed, 200)
 
 ##### End of GPIO configuration #####
 
@@ -74,60 +74,62 @@ def setup_gpios():
     Test DC/servo motors
 '''
 def self_check():
-    # Setup motor
-    wiringpi.pwmWrite(MOTOR_PWM_GPIO, 0)
-    wiringpi.digitalWrite(MOTOR_SPL_EN_GPIO, 1)
-    wiringpi.digitalWrite(MOTOR_DISABLE_GPIO, 0)
+    	# Setup motor
+	wiringpi.pwmWrite(MOTOR_PWM_GPIO, 0)
+	wiringpi.digitalWrite(MOTOR_SPL_EN_GPIO, 1)
+	wiringpi.digitalWrite(MOTOR_DISABLE_GPIO, 0)
 
-    # Servo movement
-    for angle in range(MIN_ANGLE, MAX_ANGLE, 1):
-        wiringpi.pwmWrite(SERVO_PWM_GPIO, angle)
-        time.sleep(0.01)
+    	# Servo movement
+	for angle in range(MIN_ANGLE, MAX_ANGLE, 1):
+		wiringpi.pwmWrite(SERVO_PWM_GPIO, angle)
+		print(angle)
+		time.sleep(0.5)
     
-    for angle in range(MAX_ANGLE, MIN_ANGLE, -1):
-        wiringpi.pwmWrite(SERVO_PWM_GPIO, angle)
-        time.sleep(0.01)
-    
-    # Center wheels
-    wiringpi.pwmWrite(SERVO_PWM_GPIO, int(MIN_ANGLE + (MAX_ANGLE - MIN_ANGLE) / 2))
-    
-    # Check DC motor
-    time.sleep(0.5)
-    """
-    wiringpi.digitalWrite(MOTOR_DIR_GPIO, 0)
-    wiringpi.pwmWrite(MOTOR_PWM_GPIO, SPEED)
-    time.sleep(1)
-    wiringpi.pwmWrite(MOTOR_PWM_GPIO, 0)
-    """
-<<<<<<< HEAD
-    for sp in range(0,800):
-=======
-    for sp in range(300,1500):
->>>>>>> 07728286398e92f51a6f2e21d79294882be3a8fc
-        wiringpi.digitalWrite(MOTOR_DIR_GPIO, 0)
-        wiringpi.pwmWrite(MOTOR_PWM_GPIO, sp)
-        time.sleep(0.006)
-    time.sleep(0.2)   
-    wiringpi.pwmWrite(MOTOR_PWM_GPIO, 0)
-    time.sleep(0.5) 
-    
-    wiringpi.digitalWrite(MOTOR_DIR_GPIO, 1)
-    wiringpi.pwmWrite(MOTOR_PWM_GPIO, SPEED)
-    time.sleep(1)
-    wiringpi.pwmWrite(MOTOR_PWM_GPIO, 0)
-    
-    wiringpi.digitalWrite(MOTOR_DIR_GPIO, 0)
+	for angle in range(MAX_ANGLE, MIN_ANGLE, -1):
+        	wiringpi.pwmWrite(SERVO_PWM_GPIO, angle)
+		print(angle)        
+		time.sleep(0.5)
+
+	# Center wheels
+	wiringpi.pwmWrite(SERVO_PWM_GPIO, int(MIN_ANGLE + (MAX_ANGLE - MIN_ANGLE) / 2))
+
+	# Check DC motor
+	time.sleep(0.5)
+	"""
+	wiringpi.digitalWrite(MOTOR_DIR_GPIO, 0)
+	wiringpi.pwmWrite(MOTOR_PWM_GPIO, SPEED)
+	time.sleep(1)
+	wiringpi.pwmWrite(MOTOR_PWM_GPIO, 0)
+	"""
+	#<<<<<<< HEAD
+	#    for sp in range(0,800):
+	#=======
+	for sp in range(300,1500):
+		#>>>>>>> 07728286398e92f51a6f2e21d79294882be3a8fc
+		wiringpi.digitalWrite(MOTOR_DIR_GPIO, 0)
+		wiringpi.pwmWrite(MOTOR_PWM_GPIO, sp)
+		time.sleep(0.006)
+		time.sleep(0.2)   
+		wiringpi.pwmWrite(MOTOR_PWM_GPIO, 0)
+		time.sleep(0.5) 
+
+		wiringpi.digitalWrite(MOTOR_DIR_GPIO, 1)
+		wiringpi.pwmWrite(MOTOR_PWM_GPIO, SPEED)
+		time.sleep(1)
+		wiringpi.pwmWrite(MOTOR_PWM_GPIO, 0)
+
+		wiringpi.digitalWrite(MOTOR_DIR_GPIO, 0)
 
 '''
     Button pressed event handler
 '''
 def button_pressed(pin):
-    print("Button pressed PIN: '{}'".format(pin))
+	print("Button pressed PIN: '{}'".format(pin))
 
 if __name__ == "__main__":
-    print("Configurion GPIOs")
-    setup_gpios()
+	print("Configurion GPIOs")
+	setup_gpios()
 
-    print("Self check")
-    self_check()
-    print("Self check done")
+	print("Self check")
+	self_check()
+	print("Self check done")
